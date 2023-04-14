da02d5e36612c75ecc1f8ec7c746772b6f995680
Shubham <guptashubham9829@gmail.com>
2023-04-14 13:19:36 +0530

    web 14 april

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
+  const[userData, setUserData] = React.useState([]);
+  const[userDatanochange, setUserDataNochange] = React.useState([]);
+  const[searchBoxValue, setSearchBoxValue] = React.useState();
+
+  React.useEffect(()=>{
+    console.log('hello');
+    userAllData()
+  },[])
+  const userAllData = async ()=>{
+    await fetch("https://6437c5e20c58d3b14578cb1c.mockapi.io/user/userInfo").then((res)=>{
+      return res.json()
+    }).then((data)=>{
+      setUserData(data)
+      setUserDataNochange(data)
+    })
+  };
+  const inputChange = (value) => {
+    setSearchBoxValue(value)
+    console.log(value);
+      const arr = userDatanochange.filter((ele)=>{ return ele.name.includes(value)})
+    setUserData(arr);
+    console.log(arr);
+  }
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <input value={searchBoxValue} onChange={(e)=>inputChange(e.target.value)}/>
+     {userData.length>0 &&
+      userData.map((ele,index)=>{
+        return(
+          <div className='d-flex' key={index}>
+            <img src={ele.avatar} alt="profile" height={100} width={150}/>
+            <p>{ele.name}</p>
+          </div>
+          )
+      })
+     }
     </div>
+    </>
   );
 }
 
